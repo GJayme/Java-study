5feeccfb50d37da014eb0404cda2f6cd2bce3823
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-11-01 00:19:41 -0300

    feat: desafio construtor

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -6,6 +6,18 @@ public class Data {
     int mes;
     int ano;
 
+    Data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data(int diaDesejado, int mesDesejado, int anoDesejado) {
+        dia = diaDesejado;
+        mes = mesDesejado;
+        ano = anoDesejado;
+    }
+
     String obterDataFormatada() {
         String dataFormatada = dia + "/" + mes + "/" + ano;
         return dataFormatada;
--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -3,20 +3,12 @@ package classe;
 public class DataTeste {
     public static void main(String[] args) {
 
-        Data dataNascimento = new Data();
-
-        dataNascimento.dia = 20;
-        dataNascimento.mes = 9;
-        dataNascimento.ano = 1991;
+        Data d1 = new Data();
 
         // Instanciando por eferencia!
-        var d2 = new Data();
+        var d2 = new Data(31, 12, 2020);
 
-        d2.dia = 8;
-        d2.mes = 8;
-        d2.ano = 1992;
-
-        System.out.println("Nasci no dia: " + dataNascimento.obterDataFormatada());
+        System.out.println(d1.obterDataFormatada());
         System.out.println(d2.obterDataFormatada());
     }
 }
